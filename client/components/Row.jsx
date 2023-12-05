9b59ae605f04a4d0b316bb6db3dad4c4960bf3e2
Tyler Shelton <tdshelton@gmail.com>
2023-12-05 12:23:18 -0800

    Adjust tab width to 2, clean up style/syntax errors

--- client/components/Row.jsx
+++ client/components/Row.jsx
@@ -4,7 +4,7 @@ import Square from './Square';
 const Row = () => {
   const rows = [];
   for (let i = 0; i < 100; i++) {
-    rows.push(<Square key={i}/>) 
+    rows.push(<Square key={i}/>);
   }
   return (
     <div>
