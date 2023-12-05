e27eabbf04e7f259f59667487443f67980ed379d
Tyler Shelton <tdshelton@gmail.com>
2023-12-05 14:04:18 -0800

    Add types to functions and their params

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { createServer } from 'node:http';
 import express, { Response, Request } from 'express';
-import { Server as SocketIOServer } from 'socket.io';
+import { Server as SocketIOServer, Socket } from 'socket.io';
 
 const PORT = 3000;
 
@@ -14,22 +14,35 @@ app.use(express.urlencoded());
 
 app.use('/', express.static(path.resolve(__dirname, '../dist')));
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response): void => {
   return res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'));
 });
 
 // socket.io event handling
-io.on('connection', (socket) => {
-  console.log('A user connected');
+// =========================================================================
+// 'connection' is a built in event that fires when a client
+// initiates a new websocket connection
+io.on('connection', (socket: Socket): void => {
+  console.log('A game client connected');
+
+  // `.emit()` can be used server-side to broadcast something
+  // to all connected clients, including the original sender
+  // of the 'connection' event we're currently handling.
+  //
+  // if you want to leave the original sender out, you can
+  // call `socket.broadcast.emit()`.
   socket.emit('connected', { message: 'a new client connected' });
-  socket.on('disconnect', () => {
+
+  // 'disconnect' is also a built-in event, which fires when a client
+  // connection terminates
+  socket.on('disconnect', (): void => {
     console.log('client disconnected');
   });
 });
 
-app.use((req, res) => res.status(404).send('Page Not Found'));
+app.use((req, res): Response => res.status(404).send('Page Not Found'));
 
-app.use((err: Error, req: Request, res: Response) => {
+app.use((err: Error, req: Request, res: Response): Response => {
   const defaultErr = {
     log: 'Express error handler caught unknown middleware error',
     status: 500,
