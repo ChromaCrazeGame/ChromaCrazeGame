65552831a6f3e2316bf2cd3fa64335aee9e40c7c
Tyler Shelton <tdshelton@gmail.com>
2023-12-06 11:09:46 -0800

    Set up squares to update the redux store onClick

--- client/components/Square.jsx
+++ client/components/Square.jsx
@@ -1,24 +1,15 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
 import * as actions from '../actions/actions';
+import { useSelector, useDispatch } from 'react-redux';
 
+const Square = ({ row, column }) => {
+  const color = useSelector(store => store.board.board[row][column]);
 
-const Square = (props) => {
-  // console.log(props);
-  const squareId = `r${props.row}c${props.column}`;
-  // console.log('squareId', squareId);
-  const squareColor = useSelector(store => store.board.square.color);
-  // console.log('log color state', squareColor);
   const dispatch = useDispatch();
-  const changeColor = () => dispatch(actions.changeColorActionCreator(squareId));
-  // const [color, setColor] = useState('white');
-  // const clickHandler = () => {
-  //   setColor('red');
-  // };
-
+  const handleClick = () => dispatch(actions.changeColorActionCreator(row, column, 'dodgerblue'));
 
   return (
-    <div className="square" style={ { backgroundColor: squareColor } } onClick={changeColor}></div>
+    <div className="square" style={ { backgroundColor: color } } onClick={handleClick}></div>
   );
 };
 
