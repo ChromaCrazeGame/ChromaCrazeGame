11507a3c10aa5a2945c6455cd62ec5c00cd8a5a9
Tyler Shelton <tdshelton@gmail.com>
2023-12-06 11:36:33 -0800

    Define types for managing player state

--- client/constants/actionTypes.js
+++ client/constants/actionTypes.js
@@ -1,5 +1,5 @@
 // action type constants for action creators and reducers
 
 export const CHANGE_COLOR = 'CHANGE_COLOR';
-
-
+export const PLAYER_ENTER = 'PLAYER_ENTER';
+export const PLAYER_EXIT  = 'PLAYER_EXIT';
