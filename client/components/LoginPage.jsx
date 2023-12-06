b4ec9fa0547b958898d1bf0601c6a3a79e26dd0d
Christopher <christopherlu@Christophers-MacBook-Pro.local>
2023-12-06 11:59:58 -0500

    fix syntax

--- client/components/LoginPage.jsx
+++ client/components/LoginPage.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
-    return (
-        <div>
-            <h1>Log in here chroma crazers!</h1>
-            <Link to='/gameboard'>Login</Link>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Log in here chroma crazers!</h1>
+      <Link to='/gameboard'>Login</Link>
+    </div>
+  );
 };
 
 export default LoginPage;
--- client/components/Timer.jsx
+++ client/components/Timer.jsx
@@ -2,63 +2,63 @@ import React, { useState, useEffect} from 'react';
 
 
 const Timer = () => {
-    //setting state for how many seconds we want to start game at
-    const [seconds, setSeconds] = useState(15);
-    //setting state if the game is currently running
-    const [gameRunning, setGameRunning] = useState(false);
-    //setting state to return winning team result when true
-    const [gameOver, setGameOver] = useState(false);
+  //setting state for how many seconds we want to start game at
+  const [seconds, setSeconds] = useState(15);
+  //setting state if the game is currently running
+  const [gameRunning, setGameRunning] = useState(false);
+  //setting state to return winning team result when true
+  const [gameOver, setGameOver] = useState(false);
     
-    //proof of concept to display back winner-> should be modified to the state change of winning player
-    const winner = 'The Winner is Player 1';
+  //proof of concept to display back winner-> should be modified to the state change of winning player
+  const winner = 'The Winner is Player 1';
   
-    useEffect(() => {
-        let interval;
+  useEffect(() => {
+    let interval;
   
-        if (gameRunning) {
-            interval = setInterval(() => {
-                setSeconds((prevSeconds) => {
-                    if (prevSeconds === 0) {
-                        //when timer runs out, clearing timer, changing game state and displaying result
-                        clearInterval(interval);
-                        setGameOver(true);
-                        setGameRunning(false);
-                        //pass game state in here to end game as a boolean to end game
-                        return 15; // Reset the timer to 15 seconds
-                    } else {
-                        //decrementing the timer
-                        return prevSeconds - 1;
-                    }
-                });
-            }, 1000);
-        } else {
+    if (gameRunning) {
+      interval = setInterval(() => {
+        setSeconds((prevSeconds) => {
+          if (prevSeconds === 0) {
+            //when timer runs out, clearing timer, changing game state and displaying result
             clearInterval(interval);
-        }
+            setGameOver(true);
+            setGameRunning(false);
+            //pass game state in here to end game as a boolean to end game
+            return 15; // Reset the timer to 15 seconds
+          } else {
+            //decrementing the timer
+            return prevSeconds - 1;
+          }
+        });
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
   
-        return () => clearInterval(interval);
-    }, [gameRunning]);
+    return () => clearInterval(interval);
+  }, [gameRunning]);
   
-    const startGame = () => {
-        setGameRunning(true);
-        setGameOver(false);
-        //insert function that starts gameplay
-    };
+  const startGame = () => {
+    setGameRunning(true);
+    setGameOver(false);
+    //insert function that starts gameplay
+  };
     //displaying the remaining time in counterdown
-        //if the timer is over and game has ended, displaying the winner
-        //if the game state is not over, displaying start button to start game
-    return (
-        <div>
-            <h1>Game Time Remaining: {seconds} seconds</h1>
-            {gameOver ? (
-                <>
-                    <p>Game has ended! {winner}</p>
-                    <button onClick={startGame}>Start New Game</button>
-                </>
-            ) : (
-                <button onClick={startGame}>Start Game</button>
-            )}
-        </div>
-    );
+  //if the timer is over and game has ended, displaying the winner
+  //if the game state is not over, displaying start button to start game
+  return (
+    <div>
+      <h1>Game Time Remaining: {seconds} seconds</h1>
+      {gameOver ? (
+        <>
+          <p>Game has ended! {winner}</p>
+          <button onClick={startGame}>Start New Game</button>
+        </>
+      ) : (
+        <button onClick={startGame}>Start Game</button>
+      )}
+    </div>
+  );
 };
 
 export default Timer;
