73206dfe20cb414a4d573ccf487187b6d40c396d
Tyler Shelton <tdshelton@gmail.com>
2023-12-06 11:08:53 -0800

    Render gameboard Rows based on how many are in the redux store

--- client/components/GameBoard.jsx
+++ client/components/GameBoard.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import Row from './Row';
 
-
 const GameBoard = () => {
+  const boardState = useSelector(store => store.board.board);
+
   const rows = [];
-  for (let i = 1; i <= 10; i++) {
-    rows.push(<Row key={`row${i}`} row={i}/>);
+  for (let row = 0; row < boardState.length; row++) {
+    rows.push(<Row
+      key={`row-${row}`}
+      row={row}
+    />);
   }
+
   return (
     <div id="gameboard">
       {rows}
